fb18bff01b7a3e10b70f8886d7732df460f4fef9
Steve Franz <stephenfranz22@gmail.com>
2026-09-18 08:37:16 -0400

    added game

--- main.py
+++ main.py
@@ -673,6 +673,22 @@ def generate_text_file(name, grade, current_time,testname):
     
     return send_file(file_name, as_attachment=True, mimetype="image/png", download_name=f"{name}-{session['title']}.png")
 
+@app.route("/dark-blue/<path:name>")
+def serveCoinDashFiles(name):
+    response = send_from_directory(f'./dark-blue', name)
+    response.headers.add('Cross-Origin-Opener-Policy', 'same-origin')
+    response.headers.add('Cross-Origin-Embedder-Policy', 'require-corp')
+    return response
+
+@app.route('/dark-blue')
+def coinDash():
+    response = make_response(
+        render_template('dark-blue.html'))
+    response.headers.add('Cross-Origin-Opener-Policy', 'same-origin')
+    response.headers.add('Cross-Origin-Embedder-Policy', 'require-corp')
+    return response
+
+
 @app.route("/coinDash/<path:name>")
 def serveCoinDashFiles(name):
     response = send_from_directory(f'./coinDash', name)
